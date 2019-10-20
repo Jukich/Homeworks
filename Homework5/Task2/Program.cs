188cef675d01cd90374d81d1d51eba28742af732
Jukich <51247956+Jukich@users.noreply.github.com>
2019-10-20 19:17:14 +0300

    Homework5
    Task1&2

---
+++ Homework5/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            string[] SplittedString;
+            int i = 0;
+            char[] c = { ',', '\t', ' ' };
+
+            SplittedString = str.Split(c, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Words in string:");
+
+            foreach (string s in SplittedString)
+            {
+                Console.WriteLine(i + 1 + "." + SplittedString[i]);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
