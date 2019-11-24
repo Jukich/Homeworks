ec2ad51432ebf35d8aefb28059cd9cfd14d9ff0d
Jukich <51247956+Jukich@users.noreply.github.com>
2019-11-24 16:29:43 +0200

    Homework11

---
+++ Homework11/Homework11/EqualNumbersException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    class EqualNumbersException : Exception
+    {
+        private string message = "Number is negative";
+        public override string Message { get { return message; } }
+        public EqualNumbersException() : base() { }
+        public EqualNumbersException(string message) : base(message)
+        {
+            this.message = message;
+        }
+
+    }
+    class NegativeNumberException : Exception
+    {
+        private string message = "Number is negative";
+        public override string Message { get { return message; }}
+        public NegativeNumberException() : base() { }
+        public NegativeNumberException(string message) : base(message)
+        {
+            this.message = message;
+        }
+
+    }
+}
---
+++ Homework11/Homework11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameForSpeed.Game(5);
+            int a = 3;
+            int b = 3;
+            try
+            {
+                Console.WriteLine(addition(a, b));
+            }
+            catch(EqualNumbersException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static double addition(int a, int b)
+        {
+            if (a == b)
+                throw new EqualNumbersException();
+            if (a < 0 || b < 0)
+                throw new NegativeNumberException();
+            return a + b;
+        }
+    }
+}
